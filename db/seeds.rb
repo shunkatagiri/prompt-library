a75da0be79294b502d2fa09f38e009f9e8e658ff
shunkatagiri <shun.katagiri1214@gmail.com>
2023-04-29 17:34:35 +0900

    Add models and associations for Prompt Library

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: "test@example.com", password: "password", password_confirmation: "password")
-Category.create!(name: "Category 1")
-PromptTemplate.create!(title: "Prompt 1", content: "This is a test prompt.", user_id: 1)
+# User
+user = User.create!(email: "unique_email@example.com", password: "password", password_confirmation: "password")
+
+# Category
+category = Category.create!(name: "Category 1")
+
+# PromptTemplate
+prompt_template = PromptTemplate.create!(title: "Prompt 1", content: "This is a test prompt.", user_id: user.id, category_id: category.id)
+
+# Like
+Like.create!(user_id: user.id, prompt_template_id: prompt_template.id)
+
+# Comment
+Comment.create!(content: "This is a test comment.", user_id: user.id, prompt_template_id: prompt_template.id)
