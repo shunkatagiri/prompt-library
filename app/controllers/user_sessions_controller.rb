b9ce1463eff83b8eeb8af37d8857ab752b8ba5bf
shunkatagiri <shun.katagiri1214@gmail.com>
2023-04-30 20:32:13 +0900

    Implement UserSessions controller

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,2 +1,17 @@
 class UserSessionsController < ApplicationController
+  def new; end
+
+  def create
+    if @user = login(params[:email], params[:password])
+      redirect_back_or_to(root_path, notice: 'ログインに成功しました')
+    else
+      flash.now[:alert] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, notice: 'ログアウトしました'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   
-  get 'login', to: 'user_sessions#new'
+  get 'login', to: 'user_sessions#new', as: :login
   post 'login', to: 'user_sessions#create'
-  delete 'logout', to: 'user_sessions#destroy'
+  delete 'logout', to: 'user_sessions#destroy', as: :logout
 
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
 
