cc3ca510844e239032434cd0e023ef01986450b9
shunkatagiri <shun.katagiri1214@gmail.com>
2023-05-05 18:34:45 +0900

    Add validation to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :prompt_templates
   
   validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, uniqueness: true 
 end
